f98b3758f09e9a3a8ae69b14333bbdcaa11057ad
Saurabh <kumarsyadav2@gmail.com>
2022-12-01 18:34:19 +0530

    first

---
+++ data.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<p>welcome</p>
+<?php echo $_GET["FirstName"] ?>
+<?php echo $_GET["LastName"] ?>
+<?php echo $_GET["UserName"] ?>
+<?php echo $_GET["Password"] ?>
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Resistration Form</title>
+</head>
+<body>
+  <h2>Kindly fill this form</h2>
+  <form action="data.php" method="GET"> 
+<p><label>FirstName : <input type="text" name="FirstName" /></label></p>
+<p><label>LastName : <input type="text" name="LastName" /></label></p> 
+<p><label>UserName : <input type="text" name="UserName" /></label></p>
+<p><label>Password : <input type="password" name="Password" /></label></p>
+
+<p><button type="submit" value="submit" name="submit">Submit</button></p>
+  </form>
+</body>
+</html>
