9203fa75ce700cd53eefaad0d3fbdbfabc160324
Saurabh <kumarsyadav2@gmail.com>
2022-12-07 13:05:42 +0530

    completed file handling

--- fileHandling.php
+++ fileHandling.php
@@ -1,3 +1,26 @@
 <?php
 echo readfile("webdictionary.txt");
 ?>
+
+<?php
+echo "<br/><br/>";
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
+while(!feof($myfile)) {
+  echo fgetc($myfile);
+}
+fclose($myfile);
+echo "<br/><br/>";
+?>
+
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
+echo "<br/><br/>";
+?>
+
+
