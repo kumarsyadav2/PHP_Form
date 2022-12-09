0ba1d9fd62db8fa93ed5158d2680b1008e66a3f3
Saurabh <kumarsyadav2@gmail.com>
2022-12-09 15:36:15 +0530

    modified

--- cookies.php
+++ cookies.php
@@ -5,12 +5,19 @@
 <html>
 <body>
 	<?php
-		echo "cookie is created."
+	if (isset($_COOKIE["Auction_Item"]))
+	{
+		echo "Auction Item is a " . $_COOKIE["Auction_Item"];
+	}
+	else
+	{
+		echo "No items for auction.";
+	}
 	?>
 	<p>
 		<strong>Note:</strong>
-		You might have to reload the
-		page to see the value of the cookie.
+		You might have to reload the page
+		to see the value of the cookie.
 	</p>
 
 </body>
