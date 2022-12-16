c730f951b2a83253acee11319f0ad7ff9f5c8670
Saurabh <kumarsyadav2@gmail.com>
2022-12-16 12:32:57 +0530

    code of inheritance

--- inheritance.php
+++ inheritance.php
@@ -1,3 +1,24 @@
 <?php
+class Fruit {
+    public $name;
+    public $color;
+    public function __construct($name, $color) {
+        $this->name = $name;
+        $this->color = $color;
+    }
+    public function intro() {
+        echo "The fruit is {$this->name} and the color is {$this->color}.";
+    }
+}
 
+class Strawberry extends Fruit {
+    public function message() {
+        echo "Am I a fruit or a berry? ";
+        echo "<br/>";
+    }
+}
+
+$strawberry = new Strawberry("Strawberry", "Red");
+$strawberry->message();
+$strawberry->intro();
 ?>
